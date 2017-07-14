786c8f5981951942f9345f7aaca62e9afa4e9757
vanGalilea <z.galili@icloud.com>
2017-07-14 17:08:12 +0200

    authentication and users service install

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const photos = require('./photos/photos.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(photos);
+  app.configure(users);
 };
